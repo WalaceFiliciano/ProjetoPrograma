6da50b0135ea635a9d00d08c9b4f7f9d796028d4
Walace Filiciano <walace_feliciano@hotmail.com>
2020-07-16 15:09:45 -0300

    First controller and Razor pages tests

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message:"] = " Desenvolvedor ";
+            ViewData["Developer: "] = " Walace da Silva Filiciano ";
 
             return View();
         }
