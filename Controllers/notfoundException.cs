e7e713b86cc71d510f246dae048d206c744df558
Walace Filiciano <walace_feliciano@hotmail.com>
2020-07-21 11:01:59 -0300

    Update seller and custom service exception

---
+++ Controllers/notfoundException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace SalesWebMvc.Controllers
+{
+    [Serializable]
+    internal class notfoundException : Exception
+    {
+        public notfoundException()
+        {
+        }
+
+        public notfoundException(string message) : base(message)
+        {
+        }
+
+        public notfoundException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected notfoundException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
