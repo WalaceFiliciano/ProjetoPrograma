8eb524743623637e59b8944d758dd6d71a23a757
Walace Filiciano <walace_feliciano@hotmail.com>
2020-07-21 18:02:12 -0300

    Validation

--- Models/Seller.cs
+++ Models/Seller.cs
@@ -9,17 +9,25 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+        
+        [Required(ErrorMessage ="{0} Requerido")]
+        [StringLength(60,MinimumLength = 3, ErrorMessage = "{0} tamanho deve estar entre {2} e {1}")]
         [Display(Name = "Nome")]
         public string Name { get; set; }
 
-       [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
+        [Required(ErrorMessage = "{0} Requerido")]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} Requerido")]
         [Display(Name = "Data de Aniversario")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} Requerido")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         [Display(Name = "Salario Base")]
         [DisplayFormat(DataFormatString ="{0:F2}")]
         public double BaseSalary { get; set; }
